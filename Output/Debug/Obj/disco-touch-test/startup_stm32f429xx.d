7349f45853a98641d4aab81b1becf1146d37b1c9
aholzbaur <71641117+aholzbaur@users.noreply.github.com>
2021-02-13 14:45:16 +0100

    Created SEGGER embedded studio project

---
+++ Output/Debug/Obj/disco-touch-test/startup_stm32f429xx.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/disco-touch-test/startup_stm32f429xx.o: \
+ C:\work\Projects\disco-touch-test\port\startup\startup_stm32f429xx.s
---
+++ application/main.c
@@ -0,0 +1,16 @@
+#include "stm32f4xx_ll_i2c.h"
+
+static int I2C_IsDeviceConnected(uint8_t u8Address) {
+  return 0;
+}
+
+int main(void) {
+
+  if (I2C_IsDeviceConnected(0xF1u) != 0)
+    return -2;
+
+  for (;;)
+    ;
+
+  return -1;
+}
